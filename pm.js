f85dbe1364943dfd3772df34eee2d0e4af1df8e0
Ashley Crocker <acrocker2552@gmail.com>
2021-05-17 18:44:59 -0500

    poppa mojos site

---
+++ pm.js
@@ -0,0 +1,9 @@
+document.getElementById("menu").onclick = function () {
+    location.href = "pmmenu.html";
+};
+document.getElementById("name").onclick = function () {
+    location.href = "pm.html";
+};
+document.getElementById("contact").onclick = function () {
+    location.href = "pmcontact.html";
+};
